622fdda1ae36e0061740bc0484d436bdace34833
Micah Senn <86478362+Micah-Senn@users.noreply.github.com>
2023-04-04 19:21:21 -0500

    Updated MySQL Password

--- Software2/Database/SQL.cs
+++ Software2/Database/SQL.cs
@@ -47,7 +47,7 @@ namespace Software2.Database
         }
         public static MySqlConnection GetConnection()
         {
-            string sql = "datasource=localhost;Port=3306;Username=root;Password=Xmen1029$;Database=software2";
+            string sql = "datasource=localhost;Port=3306;Username=root;Password=Passw0rd!;Database=software2";
             MySqlConnection conn = new MySqlConnection(sql);
             try
             {
